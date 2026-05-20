76dd34e1139afe3f879cc7f252857aeb255f5220
lukasdubuc <lukasdubuc@gmail.com>
2026-05-20 06:14:47 +0000

    fix: finalize router configuration for clean build

--- src/routeTree.gen.ts
+++
@@ -1,5 +0,0 @@
-import { rootRoute } from './routes/__root'
-import { indexRoute } from './routes/index'
-
-const rootRouteChildren = [indexRoute]
-export const routeTree = rootRoute.addChildren(rootRouteChildren)
