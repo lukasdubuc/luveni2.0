1cc449b914000bddcfaa8af1412dd55905d2362c
lukasdubuc <lukasdubuc@gmail.com>
2026-05-23 23:29:32 +0000

    fix: remove accidental git status text from CartContext

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -73,4 +73,4 @@ export const useCart = () => {
   const context = useContext(CartContext);
   if (!context) throw new Error("useCart must be used within a CartProvider");
   return context;
-};git status
+};
